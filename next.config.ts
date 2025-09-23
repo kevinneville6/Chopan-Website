f1d4f6831f45f29cd66a1558f336d8f0d19b7a5f
kevinneville6 <kevinneville6@gmail.com>
2025-09-24 01:45:31 +0300

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['lindy.nyc3.digitaloceanspaces.com'],
+  },
+};
+
+export default nextConfig;
